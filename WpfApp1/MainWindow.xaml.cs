80b5fe9308c67a8a5c85d9a005df0e89d0c4fbd7
Олег <@DESKTOP-GQ9LLNB>
2022-09-29 10:14:19 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+        void Metod0()
+		{
+            Result0.Visibility = Visibility.Visible;
+            Result00.Visibility = Visibility.Visible;
+        }
+
+        void Metod1()
+        {
+            Result1.Visibility = Visibility.Visible;
+            Result2.Visibility = Visibility.Visible;
+        }
+        void Metod2()
+        {
+            Result3.Visibility = Visibility.Visible;
+            Result4.Visibility = Visibility.Visible;
+        }
+        void Metod3()
+        {
+            Result5.Visibility = Visibility.Visible;
+            Result6.Visibility = Visibility.Visible;
+        }
+        void Metod4()
+        {
+            Result7.Visibility = Visibility.Visible;
+            Result8.Visibility = Visibility.Visible;
+        }
+        void Metod5()
+        {
+            Result9.Visibility = Visibility.Visible;
+            Result10.Visibility = Visibility.Visible;
+        }
+        void Metod6()
+        {
+            Result11.Visibility = Visibility.Visible;
+            Result12.Visibility = Visibility.Visible;
+        }
+        void Metod7()
+        {
+            Result13.Visibility = Visibility.Visible;
+            Result14.Visibility = Visibility.Visible;
+        }
+        void Metod8()
+        {
+            Result15.Visibility = Visibility.Visible;
+            Result16.Visibility = Visibility.Visible;
+        }
+        void Metod9()
+        {
+            Result17.Visibility = Visibility.Visible;
+            Result18.Visibility = Visibility.Visible;
+        }
+        void Metod10()
+        {
+            Result19.Visibility = Visibility.Visible;
+            Result20.Visibility = Visibility.Visible;
+        }
+        void Metod11()
+        {
+            Result21.Visibility = Visibility.Visible;
+            Result22.Visibility = Visibility.Visible;
+        }
+        void Metod12()
+        {
+            Result23.Visibility = Visibility.Visible;
+            Result24.Visibility = Visibility.Visible;
+        }
+        void Metod13()
+        {
+            Result25.Visibility = Visibility.Visible;
+            Result26.Visibility = Visibility.Visible;
+        }
+        void Metod14()
+        {
+            Result27.Visibility = Visibility.Visible;
+            Result28.Visibility = Visibility.Visible;
+        }
+        void Metod15()
+        {
+            Result29.Visibility = Visibility.Visible;
+            Result30.Visibility = Visibility.Visible;
+        }
+        void Metod16()
+        {
+            Result31.Visibility = Visibility.Visible;
+            Result32.Visibility = Visibility.Visible;
+        }
+
+
+
+        public MainWindow()
+		{
+			InitializeComponent();
+        }
+
+		string variable = TextBox.Text;
+
+
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+			{
+               switch (CBT.SelectedIndex)  // CBTreagle.SelectedIndex - индекс выбранного элемента списка
+               {
+                case 0:
+                        
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+                }
+			}
+
+            catch 
+            { 
+
+            }
+
+        }   
+	}
+}
